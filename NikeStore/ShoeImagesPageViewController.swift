712bac3a07b008e2017cf22165f5a04601b5b758
Hendrik Tekayadi <hendrik@popsical.com>
2017-05-01 00:36:43 +0800

    Add storyboardIdentifier

--- NikeStore/ShoeImagesPageViewController.swift
+++ NikeStore/ShoeImagesPageViewController.swift
@@ -26,7 +26,7 @@ class ShoeImagesPageViewController: UIPageViewController {
         
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         var controllers = [UIViewController]()
-        
+ 
         if let images = self.images {
             for image in images {
                 let shoeImageVC = storyboard.instantiateViewController(withIdentifier: Storyboard.shoeImageViewController)
@@ -82,10 +82,8 @@ class ShoeImagesPageViewController: UIPageViewController {
 
 // MARK: - UIPageViewControllerDataSource
 
-extension ShoeImagesPageViewController : UIPageViewControllerDataSource
-{
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController?
-    {
+extension ShoeImagesPageViewController : UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         if let index = controllers.index(of: viewController) {
             if index > 0 {
                 return controllers[index-1]
@@ -95,8 +93,7 @@ extension ShoeImagesPageViewController : UIPageViewControllerDataSource
         return controllers.last
     }
     
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController?
-    {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
         if let index = controllers.index(of: viewController) {
             if index < controllers.count - 1 {
                 return controllers[index + 1]
@@ -107,18 +104,16 @@ extension ShoeImagesPageViewController : UIPageViewControllerDataSource
     }
 }
 
-extension ShoeImagesPageViewController : UIPageViewControllerDelegate
-{
-    func pageViewController(_ pageViewController: UIPageViewController, willTransitionTo pendingViewControllers: [UIViewController])
-    {
+extension ShoeImagesPageViewController: UIPageViewControllerDelegate {
+    func pageViewController(_ pageViewController: UIPageViewController, willTransitionTo pendingViewControllers: [UIViewController]) {
         self.configureDisplaying(viewController: pendingViewControllers.first as! ShoeImageViewController)
     }
     
-    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool)
-    {
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
         if !completed {
             self.configureDisplaying(viewController: previousViewControllers.first as! ShoeImageViewController)
         }
     }
 }
+
 
