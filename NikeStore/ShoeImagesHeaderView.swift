ac263566c5dd594b59ec62e306943a6b2c1f7216
Hendrik Tekayadi <hendrik@popsical.com>
2017-05-01 00:44:37 +0800

    Add ShoeImagesHeaderView

---
+++ NikeStore/ShoeImagesHeaderView.swift
@@ -0,0 +1,26 @@
+//
+//  ShoeImagesHeaderView.swift
+//  NikeStore
+//
+//  Created by Popsical on 1/5/17.
+//  Copyright © 2017 htekayadi. All rights reserved.
+//
+
+import UIKit
+
+class ShoeImagesHeaderView: UIView {
+    @IBOutlet weak var pageControl: UIPageControl!
+}
+
+extension ShoeImagesHeaderView : ShoeImagesPageViewControllerDelegate {
+    func setupPageController(numberOfPages: Int) {
+        pageControl.numberOfPages = numberOfPages
+    }
+    
+    func turnPageController(to index: Int) {
+        pageControl.currentPage = index
+    }
+}
+
+
+
